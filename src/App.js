42209880f15cdecc986e9fca33b611fea0461211
聂鹏 <383990211@qq.com>
2021-03-03 00:13:38 +0800

    misc: Button start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary} size='sm'> Hello </Button>
+        <Button btnType={ButtonType.Link} href="http: //www.baidu.com" disabled> Baidu Link </Button>
       </header>
     </div>
   );
